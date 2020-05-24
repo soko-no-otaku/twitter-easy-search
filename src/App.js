0165d59044e76a301c008eebb2e4b0b79b85bf36
soko-no-otaku <soko.no.otaku@gmail.com>
2020-05-25 01:17:30 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -57,13 +57,12 @@ function App() {
           <TwitterIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          Search
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
             variant="outlined"
             margin="normal"
-            required
             fullWidth
             id="email"
             label="Email Address"
@@ -74,17 +73,12 @@ function App() {
           <TextField
             variant="outlined"
             margin="normal"
-            required
             fullWidth
             name="password"
             label="Password"
             type="password"
             id="password"
             autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
           />
           <Button
             type="submit"
@@ -93,20 +87,8 @@ function App() {
             color="primary"
             className={classes.submit}
           >
-            Sign In
+            Search
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
         </form>
       </div>
       <Box mt={8}>
