75b3c3ecb5e9915063c9e36ecb8dfcc4273627a4
soko-no-otaku <soko.no.otaku@gmail.com>
2020-05-24 20:18:06 +0900

    Update copyright

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="https://github.com/soko-no-otaku/twitter-easy-search">
+        soko-no-otaku
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
